ce51d08b364c4a2cb00cdc696ecad820ee96394e
zoekpc <zoe.martinez@kpconsulting.fr>
2025-03-20 10:43:01 +0000

    hihi

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+con = st.connection("snowflake")
